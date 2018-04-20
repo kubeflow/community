e14debf468e180e7bf28d7b095f4f2b04f188cc9
Jeremy Lewi <jeremy@lewi.us>
2018-04-19 21:26:25 -0700

    A simple script to print the names of the companies.

---
+++ scripts/print_companies.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# Print the names of all the companies listed in member organizations.yaml
+import os
+import yaml
+if __name__ == "__main__":
+  this_file = __file__
+  repo_root = os.path.abspath(os.path.join(os.path.dirname(this_file),
+                              ".."))
+  org_file = os.path.join(repo_root, "member_organizations.yaml")
+  with open(org_file) as hf:
+    members = yaml.load(hf)
+    
+  names = [m["name"] for m in members]
+  names.sort()
+  
+  print(", ".join(names))
